d17b8477d85a2cf67e63a7a42781afafbe285b4b
Davis JR <davisjr2000@hotmail.com>
2020-10-24 13:20:02 +0100

    Add copy room url to clipboard functionality

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,12 @@
 class Room < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+
     before_create :generate_room_token
+    before_create :strip_name
+
+    def strip_name
+        self.name = name.downcase.split(' ').join('-')
+    end
 
     def generate_room_token
         OpentokTokenGenerator.call(self)
