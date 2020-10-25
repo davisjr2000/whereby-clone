c4d1cca931d310083a4553db16eac673475e67b8
Davis JR <davisjr2000@hotmail.com>
2020-10-25 23:51:51 +0000

    Allow dot on url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  match '/:name' => 'rooms#show', as: :room, via: :get
-  resources :rooms, only: :create
   root to: 'pages#home'
+  match '/:name' => 'rooms#show', as: :room, via: :get, name: /.*/
+  resources :rooms, only: :create
 end
