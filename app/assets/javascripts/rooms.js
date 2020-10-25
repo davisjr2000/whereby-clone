6f5ab5442cc4ddb7457734dc1b456be7241ffd68
Davis JR <davisjr2000@hotmail.com>
2020-10-25 02:06:09 +0000

    Add new font

--- app/assets/javascripts/rooms.js
+++ app/assets/javascripts/rooms.js
@@ -1,17 +1,19 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-
 document.addEventListener('DOMContentLoaded', () => {
     buttonCopy = document.getElementById('buttonCopy');
-
-    buttonCopy.addEventListener('click', () => {
-        const textArea = document.createElement("textarea");
-        const copyText = document.querySelector('.originalUrl');
-        textArea.value = copyText.innerText.trim()
-        document.body.appendChild(textArea);
-        textArea.select();
-        textArea.setSelectionRange(0, 99999)
-        document.execCommand("copy");
-        document.body.removeChild(textArea);
-    })
+    buttonCopy.addEventListener('click', copyToClipboard)
 })
+
+const copyToClipboard = () => {
+    const textArea = document.createElement("textarea");
+    const copyText = document.querySelector('.originalUrl');
+    textArea.value = copyText.innerText.trim();
+    document.body.appendChild(textArea);
+    textArea.select();
+    textArea.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+    buttonCopy.innerHTML = "Copied!"
+    setTimeout(() => { 
+        buttonCopy.innerHTML = "Copy Link"
+    }, 3000);   
+}
