0665127995d9df777c6f319e9d997277910ab4ab
Davis JR <davisjr2000@hotmail.com>
2020-10-26 02:24:09 +0000

    Refactor code

--- app/services/opentok_token_generator.rb
+++ app/services/opentok_token_generator.rb
@@ -1,5 +1,4 @@
 class OpentokTokenGenerator < ApplicationService
-    
    require "opentok"
 
    def initialize(room)
@@ -8,7 +7,7 @@ class OpentokTokenGenerator < ApplicationService
 
    def call
     opentok = OpenTok::OpenTok.new ENV['API_KEY'], ENV['SECRET_KEY']
-    session = opentok.create_session :media_mode => :routed
+    session = opentok.create_session media_mode: :routed
     @room.session_token = session.session_id
     @room.generated_token = session.generate_token
    end
